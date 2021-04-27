7df5659128bf9555b3dc63b67f238198d0e4f67f
Talha-Shekhani <60299561+Talha-Shekhani@users.noreply.github.com>
2021-04-28 00:37:00 +0500

    T_Changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
-import  firebase from "firebase";
+import firebase from "firebase";
 var firebaseConfig = {
-    apiKey: "AIzaSyDrk9gnvo7th54NLtCfyLsn99EAgGkmYmU",
-    authDomain: "eventify-5ea05.firebaseapp.com",
-    projectId: "eventify-5ea05",
-    storageBucket: "eventify-5ea05.appspot.com",
-    messagingSenderId: "172389090599",
-    appId: "1:172389090599:web:16233fb10640df2f689502"
-  };
-  // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
-  export default firebase.database();
+  apiKey: "AIzaSyDrk9gnvo7th54NLtCfyLsn99EAgGkmYmU",
+  authDomain: "eventify-5ea05.firebaseapp.com",
+  projectId: "eventify-5ea05",
+  storageBucket: "eventify-5ea05.appspot.com",
+  messagingSenderId: "172389090599",
+  appId: "1:172389090599:web:16233fb10640df2f689502"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase.database();
