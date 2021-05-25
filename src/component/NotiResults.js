1464959dfac9886f152f850c4aedae343007015b
Ghayas <ghayas110@gmail.com>
2021-05-25 09:39:16 +0500

    Ghayas

--- src/component/NotiResults.js
+++ src/component/NotiResults.js
@@ -2,9 +2,28 @@ import React from 'react'
 import "../cssfiles/EventResults.css"
 import StarIcon from '@material-ui/icons/Star';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import { Button, ButtonBase } from '@material-ui/core';
+import { useAuth } from "../contexts/AuthContext";
+import { db, projectStorage } from "../firebase";
 function NotiResults({
     yemail,group
-}) {
+}) 
+
+{
+    const { currentUser } = useAuth()
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+       
+    
+        db.collection("notify").add(
+          {
+              currentUser:currentUser.email,
+              yemail:"ghayas110@gmail.com"
+
+          })
+          alert("Requend Sended")
+        }    
     return (
         <div className="eventResults">
             
@@ -13,7 +32,7 @@ function NotiResults({
                 <div class="event__infoTop">
                     <p>{yemail} want to talk to you Create Group </p>
                     <h3>{group}</h3>
-                 
+                 <Button variant="outlined"  onClick={handleSubmit}>Accept</Button>
 
         
 
